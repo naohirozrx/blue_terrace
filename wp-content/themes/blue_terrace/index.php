33fff6a0e4041cfc6721f2046eee6e6cdc302894
Naohiro Fukumoto <naohirozrx@yahoo.co.jp>
2024-03-14 11:01:33 +0900

    テザー公開

---
+++ wp-content/themes/blue_terrace/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>大阪 二色の浜ブルーテラス ハワイアンスタイル</title>
+  <link href="/wp-content/themes/blue_terrace/scss/style.css?<?php echo date('Ymd-Hi'); ?>" rel="stylesheet">
+</head>
+<body>
+  <div class="tessar">
+
+  </div>
+</body>
+</html>
