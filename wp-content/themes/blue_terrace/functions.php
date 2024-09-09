108ad06132da91ede7c022dd39a0019e0e518552
Naohiro Fukumoto <naohirozrx@yahoo.co.jp>
2024-09-10 01:37:57 +0900

    server

--- wp-content/themes/blue_terrace/functions.php
+++ wp-content/themes/blue_terrace/functions.php
@@ -1,5 +1,8 @@
 <?php
-  add_theme_support('post-thumbnails');
+  function my_custom_theme_setup() {
+    add_theme_support('post-thumbnails');
+  }
+  add_action( 'after_setup_theme', 'my_custom_theme_setup' );
   add_image_size( 'square', 500, 500, true );
   add_image_size( 'antore-thumbnail', 340, 222, true );
   add_image_size( 'single-thumbnail', 780, 511, true );
